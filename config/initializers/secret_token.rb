30085fe9ae3a7f47e0714ce0857f08708db1bbdb
admin <admin@localhost.localdomain>
2013-10-01 15:08:06 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HydraDemo::Application.config.secret_key_base = '61da8e72ff94d13bf0cecb854cd146551ff6f736b13fb9cd21d8600464f7d10f51f8b5541f86dcf2efcf3c6ef5917b1850e782f6b5aa5fbfe62c9bcdf47a282c'
