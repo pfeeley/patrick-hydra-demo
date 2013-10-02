20faf17cd27a0ec99208831da213582bdae651d7
admin <admin@localhost.localdomain>
2013-10-02 16:13:35 -0400

    added a pageContent datastream

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,6 +1,8 @@
 class Page < ActiveFedora::Base
   has_metadata 'descMetadata', type: PageMetadata
 
+  has_file_datastream "pageContent"
+
   belongs_to :book, :property=> :is_part_of
 
   delegate :number, to: 'descMetadata', unique: :true
