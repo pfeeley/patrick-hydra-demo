660de3aa03dc3c4fe3ff08dd662e52cf8bcd662c
admin <admin@localhost.localdomain>
2013-10-02 16:44:53 -0400

    added handling for multiple authors

--- app/models/book.rb
+++ app/models/book.rb
@@ -4,7 +4,7 @@ class Book < ActiveFedora::Base
   has_many :pages, :property => :is_part_of
 
   delegate :title, to: 'descMetadata', unique: true
-  delegate :author, to: 'descMetadata', unique: true
+  delegate :author, to: 'descMetadata', unique: false
   delegate :publication_date, to: 'descMetadata', unique: true
 
 end
